f132eb51511491d366e3550950dcc7e255634f8b
Pedro Muller <pedromullergl11@gmail.com>
2023-08-22 18:33:50 -0300

    feat: achetypes classes implemented

---
+++ src/Archetypes/Warrior.ts
@@ -0,0 +1,21 @@
+import Archetype from './Archetype';
+import { EnergyType } from '../Energy';
+
+export default class Warrior extends Archetype {
+  private _energyType: EnergyType;
+  static instanceCount = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'stamina';
+    Warrior.instanceCount += 1;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  static createdArchetypeInstances(): number {
+    return this.instanceCount;
+  }
+}
